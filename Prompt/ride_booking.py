0fe1d6f32ae13187763599296d814c88e2e92bc0
Md Mahiuddin <codermahiuddin@gmail.com>
2025-08-29 15:46:53 +0600

    template with chain

--- Prompt/ride_booking.py
+++ Prompt/ride_booking.py
@@ -7,12 +7,22 @@ model = ChatOpenAI(model="gpt-4o", temperature=1.5, max_completion_tokens=100)
 # load template from json
 template = load_prompt("./Prompt/template/template_json/ride-booking_template.json")
 # prompt
-prompt = template.invoke({
+# prompt = template.invoke({
+#     "pickup_location": "Vodra hojer Mor",
+#     "destination_location": "Padma Park",
+#     "pickup_time": "9 pm"
+# })
+
+# # model
+# response = model.invoke(prompt)
+# print(response.content)
+
+
+# Chain 
+chain = template | model
+response = chain.invoke({
     "pickup_location": "Vodra hojer Mor",
     "destination_location": "Padma Park",
     "pickup_time": "9 pm"
 })
-
-# model
-response = model.invoke(prompt)
 print(response.content)
